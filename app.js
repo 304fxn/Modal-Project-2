e8d085ea88535bae25bbb92092e11b7b8fefdc70
Florian Neacsu <78438463+304fxn@users.noreply.github.com>
2021-12-06 00:13:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+/*
+Key concepts covered:
+
+document.querySelector()
+addEventListener()
+classList.add()
+classList.remove()
+*/
+
 const main = document.querySelector(".main");
 const openmodal_btn = document.querySelector(".open-button");
 const closemodal_btn = document.querySelector(".close-modal_button");
-const closemodal = document.querySelector(".close-modal h1");
 
 openmodal_btn.addEventListener("click", function () {
   main.classList.add("myModal");
